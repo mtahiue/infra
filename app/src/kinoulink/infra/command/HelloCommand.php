a7762dc55f37e5cc13979d27a8a7449fbd7add7d
Thomas Decaux <ebuildy@gmail.com>
2015-05-02 14:08:28 +0200

    Add SF2 console application

---
+++ app/src/kinoulink/infra/command/HelloCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace kinoulink\infra\command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('knl:hello')
+            ->setDescription('Say hello world!')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello world!');
+    }
+}
